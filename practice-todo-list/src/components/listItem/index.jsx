e08a1df4e00a46b2a5b1eaf2e0e1a33f672b543a
dyehorov <114303640+dyehorov@users.noreply.github.com>
2026-02-03 11:44:49 +0100

    refactor: unify button styles in ListItem component and update layout

--- practice-todo-list/src/components/listItem/index.jsx
+++ practice-todo-list/src/components/listItem/index.jsx
@@ -4,8 +4,9 @@ export default function ListItem({ text, setTodos, id }) {
   return (
     <li className={styles.listItem}>
       <p>{text}</p>
+
       <button
-        className={styles.deleteButton}
+        className={styles.button}
         onClick={() => setTodos(prev => prev.filter(item => item.id !== id))}
       >
         <i className="fa-solid fa-xmark fa-xl"></i>
